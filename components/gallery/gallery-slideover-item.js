4b0d3cfe65b39802b56120501fd48225d79cab16
Tim Campbell <timcampbell@hey.com>
2022-10-28 11:50:10 -0400

    updates

--- components/gallery/gallery-slideover-item.js
+++ components/gallery/gallery-slideover-item.js
@@ -1,7 +1,9 @@
-import { Fragment } from 'react'
+import { Fragment, useState } from 'react'
 import { Dialog, Transition } from '@headlessui/react'
-import { XMarkIcon } from '@heroicons/react/24/outline'
+import { XMarkIcon, PlayCircleIcon } from '@heroicons/react/24/outline'
+import { PlayIcon, MusicalNoteIcon } from '@heroicons/react/20/solid'
 import Image from 'next/image'
+import Audio from '../audio'
 
 export default function GallerySlideoverItem(props) {
 
@@ -43,18 +45,11 @@ export default function GallerySlideoverItem(props) {
                     <div className="divide-y divide-gray-200">
                       <div className="pb-6">
                         <div className={"h-24 sm:h-20 lg:h-28 bg-gray-400 " + props.color} />
-                        {/* <div className="h-24 bg-[#123456] sm:h-20 lg:h-28" /> */}
                         <div className="lg:-mt-15 -mt-12 flow-root px-4 sm:-mt-8 sm:flex sm:items-end sm:px-6">
                           <div>
                             <div className="-m-1 flex">
                               <div className="inline-flex overflow-hidden rounded-lg border-4 border-white relative h-24 w-24 flex-shrink-0 sm:h-40 sm:w-40 lg:h-48 lg:w-48">
-                                <Image
-                                  className="h-24 w-24 flex-shrink-0 sm:h-40 sm:w-40 lg:h-48 lg:w-48"
-                                  src={props.src}
-                                  alt={props.alt}
-                                  layout="fill"
-                                  objectFit='cover'
-                                />
+                                <ImageLoader src={props.src} alt={props.alt} />
                               </div>
                             </div>
                           </div>
@@ -68,8 +63,9 @@ export default function GallerySlideoverItem(props) {
                             <div className="mt-5 flex-none flex-wrap space-y-3 sm:space-y-0 sm:space-x-3">
                               <button
                                 type="button"
-                                className="max-w-md items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:flex-1"
+                                className="flex max-w-md items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 pl-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:flex-1"
                               >
+                                <PlayCircleIcon className="h-6 w-6 mr-2" aria-hidden="true" />
                                 {props.cta}
                               </button>
 
@@ -80,11 +76,13 @@ export default function GallerySlideoverItem(props) {
                       <div className="px-4 py-5 sm:px-0 sm:py-0">
                         <dl className="space-y-8 sm:space-y-0 sm:divide-y sm:divide-gray-200">
                           <div className="sm:flex sm:px-6 sm:py-5">
-                            <dt className="text-sm font-medium text-gray-500 sm:w-40 sm:flex-shrink-0 lg:w-48">Summary</dt>
-                            <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0 sm:ml-6">
-                              <p>{props.desc}</p>
-                            </dd>
+                            <p className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{props.desc}</p>
                           </div>
+                          <Audio tracksource={props.tracksource1} title={props.trackname1} />
+                          <Audio tracksource={props.tracksource2} title={props.trackname2}/>
+                          <Audio tracksource={props.tracksource3} title={props.trackname3}/>
+                          <Audio tracksource={props.tracksource4} title={props.trackname4}/>
+                          <Audio tracksource={props.tracksource5} title={props.trackname5}/>
                           <div className="sm:flex sm:px-6 sm:py-5">
                             <dt className="text-sm font-medium text-gray-500 sm:w-40 sm:flex-shrink-0 lg:w-48">
                               Location
@@ -120,5 +118,27 @@ export default function GallerySlideoverItem(props) {
         </div>
       </Dialog>
     </Transition.Root>
+  )
+}
+export function ImageLoader(props) {
+  const [isLoading, setLoading] = useState(true)
+
+  function classNames(...classes) {
+    return classes.filter(Boolean).join(' ')
+  }  
+
+  return (
+    <Image
+    className={
+      isLoading
+        ? 'h-24 w-24 flex-shrink-0 sm:h-40 sm:w-40 lg:h-48 lg:w-48 transitioning'
+        : 'h-24 w-24 flex-shrink-0 sm:h-40 sm:w-40 lg:h-48 lg:w-48 transitioned'
+    }
+    onLoadingComplete={() => setLoading(false)}
+    src={props.src}
+    alt={props.alt}
+    layout="fill"
+    objectFit='cover'
+  />    
   )
 }
