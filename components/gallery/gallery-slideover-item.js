ab7f79399422494e29e50200c3c5e7a0e4a784be
Tim Campbell <timcampbell@hey.com>
2022-10-28 17:46:55 -0400

    random change to test the push structure

--- components/gallery/gallery-slideover-item.js
+++ components/gallery/gallery-slideover-item.js
@@ -85,7 +85,7 @@ export default function GallerySlideoverItem(props) {
                           <Audio tracksource={props.tracksource5} title={props.trackname5}/>
                           <div className="sm:flex sm:px-6 sm:py-5">
                             <dt className="text-sm font-medium text-gray-500 sm:w-40 sm:flex-shrink-0 lg:w-48">
-                              Location
+                              Company
                             </dt>
                             <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0 sm:ml-6">
                               New York, NY, USA
