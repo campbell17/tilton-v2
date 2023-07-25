c87ecafd7ba5ecaf3fe40bf5ccd068b8548ad299
Tim Campbell <timcampbell@hey.com>
2023-07-25 10:09:20 -0400

    add hero styling to slideover

--- components/gallery/gallery-slideover-item.js
+++ components/gallery/gallery-slideover-item.js
@@ -2,6 +2,7 @@ import { Fragment, useState } from 'react'
 import { Dialog, Transition } from '@headlessui/react'
 import { XMarkIcon, PlayCircleIcon } from '@heroicons/react/24/outline'
 import Image from 'next/image'
+import Hero from '../hero'
 import Album from '../../components/album'
 
 export default function GallerySlideoverItem(props) {
@@ -23,14 +24,19 @@ export default function GallerySlideoverItem(props) {
                 leaveTo="translate-x-full"
               >
                 <Dialog.Panel className="pointer-events-auto w-screen max-w-2xl">
-                  <div className="flex h-full flex-col overflow-y-scroll bg-white shadow-xl">
+                  <div className="flex h-full flex-col overflow-y-scroll relative shadow-xl">
+                  <Hero 
+                    darkBG
+                    heroBGStyles="absolute -z-[1] w-full h-96 bg-blue-800 py-12 bg-cover bg-right bg-[url('/images/hero.jpg')]"
+                  />
+
                     <div className="px-4 py-6 sm:px-6">
                       <div className="flex items-start justify-between">
-                        <Dialog.Title className="text-lg font-medium text-gray-900">{props.project}</Dialog.Title>
+                        <Dialog.Title className="text-lg font-medium text-gray-900"></Dialog.Title>
                         <div className="ml-3 flex h-7 items-center">
                           <button
                             type="button"
-                            className="rounded-md bg-white text-gray-400 hover:text-gray-500 focus:ring-indigo-500"
+                            className="rounded-md text-gray-400 hover:text-gray-500 focus:ring-indigo-500"
                             onClick={() => props.setOpen(false)}
                           >
                             <span className="sr-only">Close panel</span>
@@ -42,21 +48,21 @@ export default function GallerySlideoverItem(props) {
                     {/* Main */}
                     <div className="divide-y divide-gray-200">
                       <div className="pb-6">
-                        <div className={"h-24 sm:h-20 lg:h-28 " + props.color} />
-                        <div className="lg:-mt-15 -mt-12 flow-root px-4 sm:-mt-8 sm:flex sm:items-end sm:px-6">
+                        <div className="h-20" />
+                        <div className="lg:-mt-15 -mt-12 flow-root items-center px-4 sm:-mt-8 sm:flex sm:px-6">
                           <div>
                             <div className="-m-1 flex">
-                              <div className="inline-flex overflow-hidden rounded-lg border-4 border-white relative h-24 w-24 flex-shrink-0 sm:h-40 sm:w-40 lg:h-48 lg:w-48">
+                              <div className="inline-flex overflow-hidden rounded-lg border-4 border-white relative flex-shrink-0 -mt-20 sm:mt-0 h-40 w-40 lg:h-48 lg:w-48">
                                 <ImageLoader src={props.src} alt={props.alt} />
                               </div>
                             </div>
                           </div>
-                          <div className="mt-6 sm:ml-6 sm:flex-1">
+                          <div className="sm:ml-6 mt-4 sm:mt-0 text-white sm:flex-1">
                             <div>
-                              <div className="flex items-center">
-                                <h3 className="text-xl font-bold text-gray-900 sm:text-2xl">{props.project}</h3>
+                              <div className="flex">
+                                <h3 className="text-xl font-bold sm:text-2xl">{props.project}</h3>
                               </div>
-                              <p className="text-sm text-gray-500">{props.year}</p>
+                              <p className="text-sm opacity-50">{props.year}</p>
                             </div>
                             <div className="mt-5 flex-none flex-wrap space-y-3 sm:space-y-0 sm:space-x-3">
                               <a
@@ -72,7 +78,7 @@ export default function GallerySlideoverItem(props) {
                           </div>
                         </div>
                       </div>
-                      <div className="px-4 py-5 sm:px-0 sm:py-0">
+                      <div className="px-4 py-5 bg-white sm:px-0 sm:py-0">
                         <dl className="space-y-8 sm:space-y-0 sm:divide-y sm:divide-gray-200">
                           <div className="sm:flex sm:px-6 sm:py-5">
                             <p className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{props.desc}</p>
@@ -126,8 +132,8 @@ export function ImageLoader(props) {
     <Image
     className={
       isLoading
-        ? 'h-24 w-24 flex-shrink-0 sm:h-40 sm:w-40 lg:h-48 lg:w-48 transitioning'
-        : 'h-24 w-24 flex-shrink-0 sm:h-40 sm:w-40 lg:h-48 lg:w-48 transitioned'
+        ? 'flex-shrink-0 h-40 w-40 lg:h-48 lg:w-48 transitioning'
+        : 'flex-shrink-0 h-40 w-40 lg:h-48 lg:w-48 transitioned'
     }
     onLoadingComplete={() => setLoading(false)}
     src={props.src}
