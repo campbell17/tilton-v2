69301c49c714f3ac3fe3f7f0ff72167712137a52
Tim Campbell <timcampbell@hey.com>
2023-07-30 13:15:10 -0400

    add imported tracks and automate the change

---
+++ components/fringe-tracks.js
@@ -0,0 +1,15 @@
+const fringeTracks = [
+  {
+    trackName: "Newton's Last Mission",
+    trackSource: "/music/christilton_fringe_04_newtons-last-mission_excerpt.mp3",
+  },
+  {
+    trackName: "Newton's Last Mission",
+    trackSource: "/music/christilton_fringe_04_newtons-last-mission_excerpt.mp3",
+  },
+  {
+    trackName: "Newton's Last Mission",
+    trackSource: "/music/christilton_fringe_04_newtons-last-mission_excerpt.mp3",
+  }    
+]
+export default fringeTracks;
--- pages/fringe-friday.js
+++ pages/fringe-friday.js
@@ -1,8 +1,36 @@
+import { useState, useEffect } from 'react';
 import Layout from '../components/layout'
 import AudioPlayer from '../components/player/audio-player'
 import Hero from '../components/hero'
+import fringeTracks from '../components/fringe-tracks'
 
 export default function FringeFriday() {
+  const getRandomTrack = () => {
+    const randomIndex = Math.floor(Math.random() * fringeTracks.length);
+    return fringeTracks[randomIndex];
+  };
+  
+  // State to hold the selected track
+  const [selectedTrack, setSelectedTrack] = useState(getRandomTrack());
+  
+  // Function to update the selected track every Friday
+  const updateTrackOnFriday = () => {
+    const today = new Date();
+    const isFriday = today.getDay() === 5; // Friday is represented by the number 5 in JavaScript's Date object
+    
+    if (isFriday) {
+      setSelectedTrack(getRandomTrack());
+    }
+  };
+  
+  // useEffect hook to run the update function when the component mounts and every day at midnight
+  useEffect(() => {
+    updateTrackOnFriday();
+    const interval = setInterval(updateTrackOnFriday, 24 * 60 * 60 * 1000); // 24 hours interval
+    return () => clearInterval(interval);
+  }, []);
+  
+
   return (
     <Layout>
       <Hero 
@@ -11,7 +39,14 @@ export default function FringeFriday() {
         heading="Fringe Friday" 
         subheading="Take a listen to the Other Side every Friday."
       />
-      <AudioPlayer fringe notpinned="true" className="rounded-b-lg" project="Project Name" image="/images/cover-art/fringe-season-4.jpg" trackName="Newton's Last Mission" trackSource="/music/christilton_fringe_04_newtons-last-mission_excerpt.mp3"  />
+      <AudioPlayer
+        fringe
+        notpinned="true"
+        className="rounded-b-lg"
+        image="/images/cover-art/fringe-season-4.jpg"
+        trackName={selectedTrack.trackName}
+        trackSource={selectedTrack.trackSource}
+      />    
     </Layout>
   )
 }
