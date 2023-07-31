b940da4c33e0b2ae3b9cf1a1d22527dc96463a75
Tim Campbell <timcampbell@hey.com>
2023-07-30 21:37:44 -0400

    add simple icon package to use branded icons when needed, update slideover ctas

--- components/gallery/gallery-slideover-item.js
+++ components/gallery/gallery-slideover-item.js
@@ -1,6 +1,7 @@
 import { Fragment, useState } from 'react'
 import { Dialog, Transition } from '@headlessui/react'
 import { XMarkIcon, PlayCircleIcon } from '@heroicons/react/24/outline'
+import { SiSpotify } from "@icons-pack/react-simple-icons"
 import Image from 'next/image'
 import Hero from '../hero'
 import Album from '../../components/album'
@@ -73,9 +74,9 @@ export default function GallerySlideoverItem(props) {
                               <a
                                 href={props.ctalink}
                                 target="_blank noreferrer"
-                                className={`cursor-pointer flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 pl-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 hover:opacity-90 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-100 ${props.ctabrand}`}
+                                className={`cursor-pointer flex items-center justify-center rounded-md border border-transparent  px-4 py-2 pl-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 hover:opacity-90 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-100 ${props.ctabrand}`}
                               >
-                                <PlayCircleIcon className="h-6 w-6 mr-2" aria-hidden="true" />
+                                {props.cta === "Listen on Spotify" ? <SiSpotify className="h-4 w-4 mr-2 ml-1" color="#ffffff" /> : <PlayCircleIcon className="h-6 w-6 mr-2" aria-hidden="true" />}
                                 {props.cta}
                               </a>
 
