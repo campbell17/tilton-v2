829730a13339b718b5aa81976e3b6687ad9e2ac2
Tim Campbell <campbell1117@yahoo.com>
2023-08-20 19:14:22 -0400

    differentiate the FF test tracks

--- components/fringe-tracks.js
+++ components/fringe-tracks.js
@@ -1,15 +1,15 @@
 const fringeTracks = [
   {
-    trackName: "Newton's Last Mission",
-    trackSource: "/music/christilton_fringe_04_newtons-last-mission_excerpt.mp3",
+    trackName: "A New Day in the Old Town [Excerpt]",
+    trackSource: "/music/christilton_fringe_01_a-new-day-in-the-old-town_excerpt.mp3" 
+},
+  {
+    trackName: "Forest of Dreams [Excerpt]",
+    trackSource: "/music/christilton_fringe_02_forest-of-dreams_excerpt.mp3",
   },
   {
-    trackName: "Newton's Last Mission",
-    trackSource: "/music/christilton_fringe_04_newtons-last-mission_excerpt.mp3",
-  },
-  {
-    trackName: "Newton's Last Mission",
-    trackSource: "/music/christilton_fringe_04_newtons-last-mission_excerpt.mp3",
+    trackName: "Gurney for Your Thoughts [Excerpt]",
+    trackSource: "/music/christilton_fringe_03_gurney-for-your-thoughts_excerpt.mp3",
   }    
 ]
 export default fringeTracks;
