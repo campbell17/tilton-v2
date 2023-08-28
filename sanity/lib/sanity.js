774095206c98b4ef5d958624e3aa037366353b93
Tim Campbell <campbell1117@yahoo.com>
2023-08-27 21:57:21 -0400

    update env

--- sanity/lib/sanity.js
+++ sanity/lib/sanity.js
@@ -18,6 +18,7 @@ export default sanityClient({
   // Find your project ID and dataset in `sanity.json` in your studio project
   projectId: "u0ig463q",
   dataset: "production",
+  apiVersion: '2021-08-31', // use a UTC date string
   useCdn: true
   // useCdn == true gives fast, cheap responses using a globally distributed cache.
   // Set this to false if your application require the freshest possible
