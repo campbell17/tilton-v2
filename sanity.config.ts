4e6a1a2bd677f58f78ff2bd1fc0cf0c3ead8fb4e
Tim Campbell <campbell1117@yahoo.com>
2023-08-28 08:12:08 -0400

    add deplopy plugin config

--- sanity.config.ts
+++ sanity.config.ts
@@ -6,6 +6,7 @@ import {visionTool} from '@sanity/vision'
 import {defineConfig} from 'sanity'
 import {deskTool} from 'sanity/desk'
 import {colorInput} from '@sanity/color-input'
+import { vercelDeployTool } from 'sanity-plugin-vercel-deploy'
 
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
 import {apiVersion, dataset, projectId} from './sanity/env'
@@ -20,6 +21,7 @@ export default defineConfig({
   plugins: [
     deskTool(),
     colorInput(),
+    vercelDeployTool(),
     // Vision is a tool that lets you query your content with GROQ in the studio
     // https://www.sanity.io/docs/the-vision-plugin
     visionTool({defaultApiVersion: apiVersion}),
