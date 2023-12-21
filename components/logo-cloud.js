53699d9249c05262e8d3093908b7c10b8e2be7dc
Tim Campbell <campbell1117@yahoo.com>
2023-12-21 07:57:54 -0500

    add MGM & Paramount to logo cloud

--- components/logo-cloud.js
+++ components/logo-cloud.js
@@ -4,7 +4,7 @@ export default function LogoCloud() {
   return (
     <div className="bg-white py-14">
       <div className="mx-auto max-w-7xl">
-        <div className="mx-auto grid max-w-lg  items-center justify-items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-1 md:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:mr-14">
+        <div className="mx-auto grid max-w-lg  items-center justify-items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-1 md:grid-cols-5 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:mr-14">
           <div className="relative col-span-2 object-contain lg:col-span-1">
             <Image              
               src="/images/logos/logo-ea.png"
@@ -30,6 +30,22 @@ export default function LogoCloud() {
               />
           </div>
           <div className="relative col-span-2 object-contain lg:col-span-1">
+            <Image              
+              src="/images/logos/logo-mgm.png"
+              alt="MGM Plus"
+              width={114}
+              height={48}
+            />
+          </div>
+          <div className="relative col-span-2 object-contain lg:col-span-1">
+            <Image
+              src="/images/logos/logo-paramount.png"
+              alt="Parmount Plus"
+              width={102}
+              height={64}
+              />
+          </div>
+          <div className="relative col-span-2 object-contain lg:col-span-2">
             <Image
               src="/images/logos/logo-cbs.png"
               alt="CBS"
@@ -45,7 +61,7 @@ export default function LogoCloud() {
               height={64}
             />
           </div>
-          <div className="relative col-span-2 object-contain lg:col-span-1">
+          <div className="relative col-span-2 object-contain lg:col-span-2">
             <Image
               src="/images/logos/logo-ubisoft.png"
               alt="Ubisoft"
