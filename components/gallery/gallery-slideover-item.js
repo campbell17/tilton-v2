716ba36ee9b70ed2cdc2790141c6d4c382bb161e
Tim Campbell <campbell1117@yahoo.com>
2024-08-15 19:00:20 -0400

    update the tailwind config to accept Sanity colors

--- components/gallery/gallery-slideover-item.js
+++ components/gallery/gallery-slideover-item.js
@@ -79,7 +79,7 @@ export default function GallerySlideoverItem(props) {
                               <a
                                 href={props.ctalink}
                                 target="_blank noreferrer"
-                                className={`cursor-pointer flex items-center justify-center rounded-md border border-transparent px-4 py-2 ${props.ctaicon && "pl-2"} text-sm font-medium text-white shadow-sm hover:opacity-90 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-100 bg-${props.buttontype}`}
+                                className={`cursor-pointer flex items-center justify-center rounded-md border border-transparent px-4 py-2 ${props.ctaicon && "pl-2"} text-sm font-medium text-white shadow-sm hover:opacity-90 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-100 ${props.buttontype}`}
                               >
                                 {props.ctaicon === "Spotify" && <SiSpotify className="h-4 w-4 mr-2 ml-1" color="#ffffff" />}
                                 {props.ctaicon === "Apple" && <SiApplemusic className="h-4 w-4 mr-2 ml-1" color="#ffffff" />}
@@ -92,7 +92,7 @@ export default function GallerySlideoverItem(props) {
                               <a
                                 href={props.ctalinkalt}
                                 target="_blank noreferrer"
-                                className={`cursor-pointer flex items-center justify-center rounded-md border border-transparent px-4 py-2 ${props.ctaiconalt && "pl-2"} text-sm font-medium text-white shadow-sm hover:opacity-90 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-100 bg-${props.buttontypealt}`}
+                                className={`cursor-pointer flex items-center justify-center rounded-md border border-transparent px-4 py-2 ${props.ctaiconalt && "pl-2"} text-sm font-medium text-white shadow-sm hover:opacity-90 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-100 ${props.buttontypealt}`}
                               >
                                 {props.ctaiconalt === "Spotify" && <SiSpotify className="h-4 w-4 mr-2 ml-1" color="#ffffff" />}
                                 {props.ctaiconalt === "Apple" && <SiApplemusic className="h-4 w-4 mr-2 ml-1" color="#ffffff" />}
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,19 +3,20 @@ module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./sanity/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
+      colors: {
+        applemusic: '#fc3c44',
+        mgm: '#c7a338',
+        paramount: '#0164ff',
+        spotify: '#1dB954',
+      },
       transitionProperty: {
         'height': 'height',
         'rotate': 'transform',
       },      
-      colors: {
-        'apple-music': '#fc3c44',
-        'mgm': '#c7a338',
-        'paramount': '#0164ff',
-        'spotify': '#1dB954',
-      },
     },
   },
   plugins: [],
